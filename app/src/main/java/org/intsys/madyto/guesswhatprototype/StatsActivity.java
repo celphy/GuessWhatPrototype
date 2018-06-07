96b5b547004293423158239e72c0ed7322af15d7
marle <marleen.eisenacht@gmx.de>
2018-06-07 18:30:50 +0200

    sth changed statsactivity

--- app/src/main/java/org/intsys/madyto/guesswhatprototype/StatsActivity.java
+++ app/src/main/java/org/intsys/madyto/guesswhatprototype/StatsActivity.java
@@ -3,7 +3,7 @@ package org.intsys.madyto.guesswhatprototype;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class activity_stats extends AppCompatActivity {
+public class StatsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
