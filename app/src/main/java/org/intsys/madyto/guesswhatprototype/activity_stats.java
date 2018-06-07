989134725dea2e5bf05700f1be1fa17c3ec6c0ed
and1kl <andreasklaiber@web.de>
2018-06-07 18:05:34 +0200

    comment added

--- app/src/main/java/org/intsys/madyto/guesswhatprototype/activity_stats.java
+++ app/src/main/java/org/intsys/madyto/guesswhatprototype/activity_stats.java
@@ -9,5 +9,6 @@ public class activity_stats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        // Das ist ein Test
     }
 }
