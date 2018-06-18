0c6548549ef6221f7507f080ed2c8e72a7ae9643
and1kl <andreasklaiber@web.de>
2018-06-18 12:33:57 +0200

    activity_stats transparent button + onClickListener added

--- app/src/main/java/org/intsys/madyto/guesswhatprototype/StatsActivity.java
+++ app/src/main/java/org/intsys/madyto/guesswhatprototype/StatsActivity.java
@@ -1,7 +1,10 @@
 package org.intsys.madyto.guesswhatprototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -9,6 +12,15 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        // Das ist ein Test
+
+
+        Button rtnMainBtn = (Button) findViewById(R.id.btnReturnMain);
+        rtnMainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMainIntent = new Intent(StatsActivity.this, MainActivity.class);
+                StatsActivity.this.startActivity(goToMainIntent);
+            }
+        });
     }
 }
