4206f453e5b5706b56623ce3a4dbfaa7eec48e43
celphy <db.darkrib@googlemail.com>
2018-06-18 11:47:06 +0200

    Added newGameButton functionality launch statsPage TEMP

--- app/src/main/java/org/intsys/madyto/guesswhatprototype/MainActivity.java
+++ app/src/main/java/org/intsys/madyto/guesswhatprototype/MainActivity.java
@@ -1,13 +1,31 @@
 package org.intsys.madyto.guesswhatprototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent gotoSettingsIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGameButton = (Button)findViewById(R.id.btnNewGame);
+        gotoSettingsIntent = new Intent(MainActivity.this, StatsActivity.class);
+
+        newGameButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                MainActivity.this.startActivity(gotoSettingsIntent);
+            }
+        });
+
+
     }
 }
