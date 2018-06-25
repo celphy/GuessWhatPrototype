439bd9e39820b2113b4ae1ed7f79ac53736453c6
and1kl <email.andreasklaiber@gmail.com>
2018-06-25 22:09:56 +0200

    bg_currentgame_after added activity_currentgame_after added CurrentgameActivityAfter added

---
+++ app/src/main/java/org/intsys/madyto/guesswhatprototype/CurrentgameActivityAfter.java
@@ -0,0 +1,64 @@
+package org.intsys.madyto.guesswhatprototype;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class CurrentgameActivityAfter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currentgame);
+
+        Button rtnMainBtn = (Button) findViewById(R.id.btnReturnMain);
+        rtnMainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        Button highscoreBtn = (Button) findViewById(R.id.btnHighscore);
+        highscoreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToHighscoreIntent = new Intent( CurrentgameActivityAfter.this, HighscoreActivity.class);
+                CurrentgameActivityAfter.this.startActivity(goToHighscoreIntent);
+            }
+        });
+
+
+        Button settingsBtn = (Button) findViewById(R.id.btnSettings);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToSettingsIntent = new Intent(CurrentgameActivityAfter.this, SettingsActivity.class);
+                CurrentgameActivityAfter.this.startActivity(goToSettingsIntent);
+            }
+        });
+
+        Button playCurrentGameButton = (Button) findViewById(R.id.btnCurrentGamePlay);
+        playCurrentGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToIngameIntent = new Intent(
+                        CurrentgameActivityAfter.this,
+                        IngameActivity.class);
+                CurrentgameActivityAfter.this.startActivity(goToIngameIntent);
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        Intent goToMainActivityItent = new Intent(
+                CurrentgameActivityAfter.this,
+                MainActivity.class);
+        CurrentgameActivityAfter.this.startActivity(goToMainActivityItent);
+    }
+}
